ae8b9c44ee3284e91e18a44420c71693604d7b1f
nerea <nerea@ubuntu.myguest.virtualbox.org>
2023-11-30 19:00:36 +0100

    currency acabado

--- src/components/Currency.vue
+++ src/components/Currency.vue
@@ -1,7 +1,23 @@
 <template>
-
+    <div class="segundoComponente">
+        <span>Currency</span>
+        <select>
+            <option>Dollars($)</option>
+            <option>Euros(€)</option>
+        </select>
+    </div>
 </template>
 
 <script setup lang="ts">
 
 </script>
+
+<style scoped>
+.segundoComponente{
+    text-align: center;
+    margin-top: 10px;
+}
+select{
+    margin-left: 10px;
+}
+</style>
