05d9dde8c7a722fdd4b5008154326de78f8532e7
nerea <nerea@ubuntu.myguest.virtualbox.org>
2023-11-30 18:46:18 +0100

    input hecho

---
+++ comandas/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import currency from "@/components/Currency.vue";
+import { ref } from "vue";
+const text = ref('')
+
+</script>
+
+<template>
+  <div class="principalComponente">
+    <h1>{{ text }}</h1>
+    <input v-model="text">
+    <!-- Aqui hacemos que el boton muestre un alert confome se ejecutado -->
+    <button onclick="alert('Your order has been placed!');">Place order</button>
+  </div>
+  <currency></currency>
+</template>
+
+<style scoped>
+.principalComponente{
+  text-align: center;
+  margin-top: 250px;
+}
+</style>
